17f5f08f8333bc6cfcd2d5c5ff23d78d586c14ee
Iryna Shevkun <sevkuniryna65@gmail.com>
2025-01-22 12:45:37 +0200

    fix issues

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,33 +11,27 @@ function transformStateWithClones(state, actions) {
   const result = [];
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      for (const key in stateCopy) {
-        delete stateCopy[key];
-      }
+    switch(action.type) {
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
 
-      const copy = { ...stateCopy };
+      case 'addProperties':
+        Object.assign(stateCopy, { ...action.extraData });
+        break;
 
-      result.push(copy);
+      default:
+        for (let key = 0; key < action.keysToRemove.length; key++) {
+          delete stateCopy[action.keysToRemove[key]];
+        }
+        break;
     }
 
-    if (action.type === 'addProperties') {
-      Object.assign(stateCopy, { ...action.extraData });
+    const copy = { ...stateCopy };
 
-      const copy = { ...stateCopy };
-
-      result.push(copy);
-    }
-
-    if (action.type === 'removeProperties') {
-      for (let key = 0; key < action.keysToRemove.length; key++) {
-        delete stateCopy[action.keysToRemove[key]];
-      }
-
-      const copy = { ...stateCopy };
-
-      result.push(copy);
-    }
+    result.push(copy);
   }
 
   return result;
